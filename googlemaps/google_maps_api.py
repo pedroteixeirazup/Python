a60d9128301bf5ecddec5fd793162e9cc5c17a36
Pedro Henrique Faria Teixeira <42281088+pedroteixeirazup@users.noreply.github.com>
2019-09-11 16:40:14 -0300

    Update google_maps_api.py

--- googlemaps/google_maps_api.py
+++ googlemaps/google_maps_api.py
@@ -2,7 +2,7 @@
 import googlemaps 
   
 # Requires API key 
-gmaps = googlemaps.Client(key='AIzaSyBQ5dcUgP4E9ZS_Dy59XcmpRd3M91DOh6U') 
+gmaps = googlemaps.Client(key='YOUR KEY') 
   
 # # Requires cities name 
 # my_dist = gmaps.distance_matrix('40.65454,-73.5454','40.654465,-80.54564')['rows'][0]['elements'][0] 
