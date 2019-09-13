178e7051b5e8256aca5a7ef64f44e5c980284fa7
pedroteixeirazup <pedro.teixeira@zup.com.br>
2019-09-13 10:55:58 -0300

    Resolve all problems gcp.

--- googlemaps/google_maps_api.py
+++ googlemaps/google_maps_api.py
@@ -1,23 +1,51 @@
-# importing googlemaps module 
 import googlemaps 
-  
+
+'''Distance Matrix API'''  
 # Requires API key 
-gmaps = googlemaps.Client(key='YOUR KEY') 
+gmaps = googlemaps.Client(key='YOUR-KEY') 
   
-# # Requires cities name 
-# my_dist = gmaps.distance_matrix('40.65454,-73.5454','40.654465,-80.54564')['rows'][0]['elements'][0] 
-
-def getlatlong(origin, destiny):
+# Solve problems 1 and 2 
+def time_distance(origin, destiny):
     my_dist = gmaps.distance_matrix(origin,destiny)['rows'][0]['elements'][0] 
-    print(my_dist) 
+    print(my_dist['duration']) 
+    # return  my_dist['duration']['text']
 
-# # Printing the result 
-# print(my_dist)
-print('Enter with lat and long of your origin: ') 
-origin = raw_input()
+# Inputs got distance matrix api
+# print('Enter with lat and long of your origin: ') 
+# origin = input()
 
-print('Enter with lat and long of your destination: ')
-destiny = raw_input()
+# print('Enter with lat and long of your destination: ')
+# destiny = input()
 
-getlatlong(origin,destiny)
+# time_distance('Araguari','Paracatu')
 
+
+# Solve porblem 3
+def check_radius(position1, position2, radius):
+    data = gmaps.distance_matrix(position1,position2)['rows'][0]['elements'][0]
+    distance_between = data['distance']['text'].split()
+
+    if float(distance_between[0]) > float(radius):
+        return False
+    
+    return True
+
+# print(check_radius('40.654564, -73.6565','40.564564, -73.6565',30))
+# print(check_radius('Sao Paulo','Uberlandia',30))
+
+
+
+#Solve problem 4
+def coord_highway(km_init, km_finish, highway):
+    x = 0
+    list_lat_long = []
+
+    for i in range(km_init, km_finish + 1):
+        address = 'km ' + str(i) + ' ' + highway
+        data = gmaps.geocode(address)
+        list_lat_long.append(data[0]['geometry']['location'])
+
+    return list_lat_long 
+
+lista = coord_highway(217, 236, 'SP-052')
+print(lista)
